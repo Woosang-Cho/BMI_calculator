fe958f27c042af7194b778719fead990d672b777
Woosang-Cho <woosang05@naver.com>
2025-03-28 12:53:06 +0900

    비만도 판정 추가

--- BMI_calculator/bmi_calculator.py
+++ BMI_calculator/bmi_calculator.py
@@ -14,4 +14,17 @@ weight, height_cm = func_BMI()
 
 BMI = weight / (height_cm * height_cm)
 
-print(f"BMI: {BMI:.2f}")
+print(f"BMI: {BMI:.2f} \n")
+
+if BMI < 18.5:
+    print('저체중')
+elif 18.5 <= BMI < 23:
+    print('정상')
+elif 23 <= BMI < 25:
+    print('비만 전 단계 (과체중 or 위험 체중)')
+elif 25 <= BMI < 30:
+    print('1단계 비만')
+elif 30 <= BMI < 35:
+    print('2단계 비만')
+else:
+    print('단계 비만')
